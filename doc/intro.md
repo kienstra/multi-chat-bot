3ad324ee12481a3e4392582c4960043508b8c74f
Ryan Kienstra <kienstraryan@gmail.com>
2024-01-31 10:37:54 -0600

    Create new repo from seancorfield/deps-new
    Comand:
    clojure -Tnew app :name kienstra/multi-chat-bot

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kienstra/multi-chat-bot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kienstra/multi_chat_bot_test.clj
@@ -0,0 +1,7 @@
+(ns kienstra.multi-chat-bot-test
+  (:require [clojure.test :refer :all]
+            [kienstra.multi-chat-bot :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
